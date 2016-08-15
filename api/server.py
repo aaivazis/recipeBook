c93e93bbba5b535057470ac9c7e7d6e76edd6bb3
Alec Aivazis <alec@aivazis.com>
2016-08-15 09:57:37 -0700

    added api auth example

--- api/server.py
+++ api/server.py
@@ -2,4 +2,9 @@
 import nautilus
 
 # create a nautilus service with just the schema
-class RecipeBookAPI(nautilus.APIGateway): pass
+class RecipeBookAPI(nautilus.APIGateway):
+
+    @nautilus.auth_criteria('recipe')
+    async def auth_recipe(self, model, user_id):
+        # only recipes with id 2 are visible
+        return await model._has_id(2)
